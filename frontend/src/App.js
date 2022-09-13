3c8b328713a9254ccd17586d1e4c5e62deea78c9
Saad-Lamrini <saadlamrini20@gmail.com>
2022-09-14 00:46:45 +0100

    adding react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import data from './data';
+//import data from './data';
 
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Homescreen from './Screens/Homescreen';
 import Productscreen from './Screens/Productscreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Boutique Lamrini</Link>
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Boutique lamrini</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<Productscreen />} />
-            <Route path="/" element={<Homescreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<Productscreen />} />
+              <Route path="/" element={<Homescreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
