b5562c587342064ac4677a4689025551e3d638bc
Saad-Lamrini <saadlamrini20@gmail.com>
2022-09-17 11:35:34 +0100

    creating react context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,18 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Homescreen from './Screens/Homescreen';
 import Productscreen from './Screens/Productscreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import Store from './Store';
+//import Badge from 'react-bootstrap/esm/Badge';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -19,6 +27,16 @@ function App() {
                   <h3>BOUTIQUE LAMRINI</h3>
                 </Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
